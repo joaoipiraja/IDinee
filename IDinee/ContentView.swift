220da636da8b21a4d44db3b061acc3b6dbb44a55
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2021-01-07 13:51:55 -0300

    Initial Commit

---
+++ IDinee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IDinee
+//
+//  Created by João Victor Ipirajá de Alencar on 07/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ IDinee/IDineeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineeApp.swift
+//  IDinee
+//
+//  Created by João Victor Ipirajá de Alencar on 07/01/21.
+//
+
+import SwiftUI
+
+@main
+struct IDineeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
